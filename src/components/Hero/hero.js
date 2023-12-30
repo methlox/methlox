f0257ca3ecec1104cb8ae180ff517faa65bd336d
methlox <astroyellowh@gmail.com>
2023-12-30 22:11:28 +0530

    hero bg v3

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import hiro from '../../assets/hero.jpg'
 import './hero.css'
 
 function hero() {
   return (
-    <div>
-        <img className='bg' src={hiro} alt='hero' />
+    <div className='body'>
+        
     </div>
   )
 }
