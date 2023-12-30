88c126dfe2a0c23df91cdc9be32e5e5edf50e742
methlox <astroyellowh@gmail.com>
2023-12-30 17:41:33 +0530

    navbar v1

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState} from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import './Navbar.css';
+// import logo from "../../public/me.png";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          {/* <img src={logo} alt="logo" /> */}
+          <h4>Siddhant Sharma</h4>
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <div
+          className={`${
+            showLinks ? "links-container show-container" : "links-container"
+          }`}
+        >
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((link) => {
+            const { id, url, icon } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
