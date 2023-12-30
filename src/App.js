f1b46b70747bb9b0165e8c6a80f3ff2d5bdefa57
methlox <astroyellowh@gmail.com>
2023-12-30 17:57:26 +0530

    hero bg v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Hero from './components/hero'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import hiro from '../assets/hero.jpg'
+
+function hero() {
+  return (
+    <div>
+        <img src={hiro} alt='hero' />
+    </div>
+  )
+}
+
+export default hero
