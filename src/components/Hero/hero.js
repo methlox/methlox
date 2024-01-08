adc94b6e389a8cc2b3ece10d0be9d00a07490b64
methlox <astroyellowh@gmail.com>
2024-01-08 15:46:49 +0530

    tried and failed

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import './hero.css'
-import chevdown from '../../assets/images/chevdown.svg'
+import React from "react";
+import "./hero.css";
+import chevdown from "../../assets/images/chevdown.svg";
+// import Sketch from "../../components/sketch/sketch";
+import dev from "../../assets/images/dev.jpg";
+import devgif from "../../assets/images/dev.gif";
+import des from "../../assets/images/des.jpg";
+import desgif from "../../assets/images/des.gif";
+import dominic from "../../assets/sketches/dominic.png";
+import brock from "../../assets/sketches/brock.png";
 
 function hero() {
   return (
     <div>
-      <img src={chevdown} className='container'></img>
+      <img src={chevdown} className="icon"></img>
     </div>
-  )
+  );
 }
 
-export default hero
+export default hero;
