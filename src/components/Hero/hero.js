b37a9f804ecc6d06774b79c3575cb39a5a22dadc
methlox <astroyellowh@gmail.com>
2024-01-08 18:47:10 +0530

    about section v1

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,18 +1,50 @@
 import React from "react";
-import "./hero.css";
+import "./hero.scss";
 import chevdown from "../../assets/images/chevdown.svg";
-// import Sketch from "../../components/sketch/sketch";
-import dev from "../../assets/images/dev.jpg";
-import devgif from "../../assets/images/dev.gif";
-import des from "../../assets/images/des.jpg";
-import desgif from "../../assets/images/des.gif";
-import dominic from "../../assets/sketches/dominic.png";
-import brock from "../../assets/sketches/brock.png";
 
 function hero() {
   return (
     <div>
+      {/* <link rel="stylesheet" href="devicon.min.css"></link> */}
       <img src={chevdown} className="icon"></img>
+      <div className="container">
+        <div class="user-details">
+          <h1> My Expertise </h1>
+          <p>hi</p>
+        </div>
+        <div class="user">
+          <div class="tech">
+            <h2>Design</h2>
+            <i class="devicon-html5-plain colored"></i>
+            <i class="devicon-bootstrap-plain-wordmark colored"></i>
+            <i class="devicon-sass-original colored"></i>
+            <p>
+              Mumblecore hexagon kombucha, pitchfork four loko raclette
+              intelligentsia master cleanse. Vinyl XOXO lumbersexual
+            </p>
+          </div>
+          <div class="tech">
+            <h2>Code</h2>
+            <i class="devicon-javascript-plain colored"></i>
+            <i class="devicon-react-original-wordmark colored"></i>
+            <i class="devicon-nodejs-plain-wordmark"></i>
+            <p>
+              Mumblecore hexagon kombucha, pitchfork four loko raclette
+              intelligentsia master cleanse. Vinyl XOXO lumbersexual
+            </p>
+          </div>
+          <div class="tech">
+            <h2>Tools</h2>
+            <i class="devicon-git-plain"></i>
+            <i class="devicon-gulp-plain colored"></i>
+            <i class="devicon-atom-original-wordmark"></i>
+            <p>
+              Mumblecore hexagon kombucha, pitchfork four loko raclette
+              intelligentsia master cleanse. Vinyl XOXO lumbersexual
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
